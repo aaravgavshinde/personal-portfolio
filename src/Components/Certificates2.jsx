1eeb1a11150f027e328778f7b782c32b0ab56f6f
aaravgavshinde <aaravgavshinde@gmail.com>
2024-01-27 18:41:39 +0530

    added certi links

--- src/Components/Certificates2.jsx
+++ src/Components/Certificates2.jsx
@@ -13,7 +13,7 @@ export default function Certificates2({ componentRef }) {
                         <div> Digital Savers Pvt. Ltd. </div>
                     </div>
                     <div className='certi-2'>
-                        <a href="">See PDF</a>
+                        <a href="https://drive.google.com/file/d/1ONqdl61r5axPfGRbx04j5Yr9wTevWlLL/view?usp=drive_link" target='_blank'>See PDF</a>
                     </div>
                 </div>
                 <div className='certificate'>
@@ -22,7 +22,7 @@ export default function Certificates2({ componentRef }) {
                         <div> Digital Savers Pvt. Ltd. </div>
                     </div>
                     <div className='certi-2'>
-                        <a href="">See PDF</a>
+                        <a href="https://drive.google.com/file/d/1zbj1I_3n8Fj96Sz_DDO4BW2qFyPFZOOG/view?usp=drive_link" target='_blank'>See PDF</a>
                     </div>
                 </div>
                 <div className='certificate'>
@@ -31,7 +31,16 @@ export default function Certificates2({ componentRef }) {
                         <div> Digital Savers Pvt. Ltd. </div>
                     </div>
                     <div className='certi-2'>
-                        <a href="">See PDF</a>
+                        <a href="https://drive.google.com/file/d/1edEV9rslCRB2c3Ru-ATCnXLEUR1TCk4C/view?usp=drive_link" target='_blank'>See PDF</a>
+                    </div>
+                </div>
+                <div className='certificate'>
+                    <div className='certi-1'>
+                        <div> <span>MERN Stack</span> Letter of Completion </div>
+                        <div> Digital Savers Pvt. Ltd. </div>
+                    </div>
+                    <div className='certi-2'>
+                        <a href="https://drive.google.com/file/d/1qxenorIYg9tJmTJ8Kw0jLp3kEbnAgJC0/view?usp=drive_link" target='_blank'>See PDF</a>
                     </div>
                 </div>
                 <div className='certificate'>
@@ -40,7 +49,7 @@ export default function Certificates2({ componentRef }) {
                         <div> NxtWave </div>
                     </div>
                     <div className='certi-2'>
-                        <a href="">See PDF</a>
+                        <a href="https://drive.google.com/file/d/1sNDIn9z8ylNwDnusXHy46gOjI4Fud_pD/view?usp=drive_link" target='_blank'>See PDF</a>
                     </div>
                 </div>
             </div>
